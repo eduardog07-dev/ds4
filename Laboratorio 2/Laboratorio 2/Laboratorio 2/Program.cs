4d89c1d8e93e85bd002d9b3b33373ec2e6159f21
Marmolejo-eduardo <marmolejoeduardo8@gmail.com>
2025-09-16 19:02:56 -0500

    Laboratorio 2

---
+++ Laboratorio 2/Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Client client = new Client();
+            //Ejmplo utilizando las variables de instancia de Clase.
+            client.FirstName = "Eduardo";
+            client.LastName = "Marmolejo";
+            client.Age = 24;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+        //Declaradando variables de instancia en clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            //Utilizando variable de instancia en metodo de la clase.
+            return FirstName + "" + LastName;
+        }
+    }
+}
