a022dde8b26c525c86d143ff69f4c4e0f1f68001
Marmolejo-eduardo <marmolejoeduardo8@gmail.com>
2025-09-16 19:20:56 -0500

    laboratorio21

---
+++ Laboratorio 2/Laboratorio 2/Laboratorio21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Asignando valor a variable estatica.
+            MyClass.valor = 1;
+            Console.WriteLine(MyClass.valor);
+        }
+    }
+    public class MyClass
+    {
+        //Declarando variable estatica.
+        public static int valor;
+    }
+}
