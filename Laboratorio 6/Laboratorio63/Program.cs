22a298286b55f9c32aae344ca814eee7372a79ae
Marmolejo-eduardo <marmolejoeduardo8@gmail.com>
2025-09-23 23:46:47 -0500

    Laboratorio6

---
+++ Laboratorio 6/Laboratorio63/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main (string[] args)
+    {
+        try
+        {
+            int[] myNumbers = {1, 2, 3};
+            Console.WriteLine(myNumbers[10]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Algo salio mal, valida el indice del arreglo");
+        }
+        finally
+        {
+            Console.WriteLine("Continuación de la aplicacion, luego del bloque try/catch");
+        }
+    }
+}
---
+++ Laboratorio 6/Laboratorio64/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 10)
+        {
+            throw new ArithmeticException("Acceso negado - no cumple con el criteio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso Concedido");
+        }
+    }
+    static void Main (string[] args)
+    {
+        checkAge(15);
+    }
+}
