2272650af1809da3563165c014d12adb44d8f1a0
Marmolejo-eduardo <marmolejoeduardo8@gmail.com>
2025-10-01 19:25:20 -0500

    Laboratorio7

---
+++ Laboratorio 7/Laboratorio71/Laboratorio71/Program.cs
@@ -0,0 +1,14 @@
+﻿
+/*static void Main(string[] args)
+{
+    Banco banco1 = new Banco();
+    banco1.Operar();
+    banco1.DepositosTotales();
+    Console.ReadKey();
+}*/
+
+// De esta forma si me mostró el resultado dado en la hoja de laboratorio
+Banco banco1 = new Banco();
+banco1.Operar();
+banco1.DepositosTotales();
+Console.ReadKey();
---
+++ Laboratorio 7/Laboratorio72/Program.cs
@@ -0,0 +1,15 @@
+﻿/*static void Main(string[] args)
+{
+    JuegoDeDados j = new JuegoDeDados();
+    j.Jugar();
+}
+*/
+
+
+// De esta forma si me mostró el resultado dado en la hoja de laboratorio
+
+JuegoDeDados j = new JuegoDeDados();
+j.Jugar();
+
+// Mantiene la consola abierta hasta que presiones una tecla
+Console.ReadKey();
