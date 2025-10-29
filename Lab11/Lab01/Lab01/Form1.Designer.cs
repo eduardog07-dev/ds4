2ec68f5c9f783f4179f4d7e3fa63a7f59a1dfc88
Marmolejo-eduardo <marmolejoeduardo8@gmail.com>
2025-10-29 18:37:38 -0500

    ProyectoCarluladora

---
+++ Lab11/Lab01/Lab01/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Lab01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblMundo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(352, 184);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click This";
+            this.btnClick.UseVisualStyleBackColor = true;
+            // 
+            // lblMundo
+            // 
+            this.lblMundo.AutoSize = true;
+            this.lblMundo.Location = new System.Drawing.Point(370, 260);
+            this.lblMundo.Name = "lblMundo";
+            this.lblMundo.Size = new System.Drawing.Size(44, 16);
+            this.lblMundo.TabIndex = 1;
+            this.lblMundo.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMundo);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblMundo;
+    }
+}
+
---
+++ Lab11/Lab01/Lab01/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    // Todo este archivo es tu clase Form1
+    public partial class Form1 : Form
+    {
+        // Este es el CONSTRUCTOR.
+        // Se ejecuta UNA SOLA VEZ cuando la ventana se crea.
+        public Form1()
+        {
+            InitializeComponent();
+        } // Aquí termina el constructor.
+
+        // Este es el MANEJADOR DE EVENTOS del botón.
+        // Se ejecuta CADA VEZ que se hace clic en el botón.
+        // Debe estar aquí, fuera del constructor pero dentro de la clase Form1.
+        private void btn_Click(object sender, EventArgs e)
+        {
+            lblMundo.Text = "Hola,Mundo";
+        }
+    } // Aquí termina la clase Form1.
+}
