f084e8042f1b88feee20d101b552bb8b0129d1b5
Marmolejo-eduardo <marmolejoeduardo8@gmail.com>
2025-11-11 20:48:02 -0500

    Laboratorio15

---
+++ Laboratorio 15/Laboratorio15-4/Laboratorio15-4/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Laboratorio15_4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Introduzca dos valores a sumar"></asp:Label>
+        </div>
+        <p>
+            <asp:TextBox ID="txtb1" runat="server">valor1</asp:TextBox>
+            <asp:TextBox ID="txtb2" runat="server">valor2</asp:TextBox>
+            <asp:Button ID="btnSumar" runat="server" OnClick="Button1_Click" Text="Realizar Suma" />
+        </p>
+        <asp:Label ID="lblResultado" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Laboratorio 15/Laboratorio15-4/Laboratorio15-4/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laboratorio15_4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2;
+
+            // Intentamos convertir los valores de los TextBox
+            bool exito1 = double.TryParse(txtb1.Text, out valor1);
+            bool exito2 = double.TryParse(txtb2.Text, out valor2);
+
+            if (exito1 && exito2)
+            {
+                // Sumar los valores
+                double suma = valor1 + valor2;
+
+                // Mostrar el resultado en el Label
+                lblResultado.Text = "El valor sumado es = " + suma.ToString();
+            }
+            else
+            {
+                // Mostrar error en el Label si hay un valor inválido
+                lblResultado.Text = "Por favor ingrese números válidos en ambos campos.";
+            }
+
+        }
+
+    }
+}
